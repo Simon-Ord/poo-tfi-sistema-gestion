c42c0a8f4372a2436d9c2c0dade24229b88c1c74
Simon-Ord <saraimon2@gmail.com>
2025-09-14 15:10:48 -0300

    Probando primeros cambios

---
+++ sistema-gestion-mundoPC/src/main/java/com/mycompany/sistema/gestion/mundopc/SistemaGestionMundoPC.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.sistema.gestion.mundopc;
+
+public class SistemaGestionMundoPC {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ sistema-gestion-mundoPC/src/main/java/modelo/Producto.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Producto {
+
+private String codigo;    
+private String nombre;
+private String descripcion;
+private Double precio;
+private Integer stock;
+
+
+}
