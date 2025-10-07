ed5f8a387e94cb69e0a00d71bc6336d80f00e31c
Alexis coronel <ac9916152@gmail.com>
2025-10-07 16:51:13 -0300

    creo la bases de datos y el tipo usuario

--- sistema-gestion-computacion/src/main/java/com/unpsjb/poo/controller/LoginViewController.java
+++ sistema-gestion-computacion/src/main/java/com/unpsjb/poo/controller/LoginViewController.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-
 public class LoginViewController implements Initializable {
 
     @FXML
